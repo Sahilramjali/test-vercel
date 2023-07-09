6f22e04fecb3b221e56c5fb408d575c0dd8b5ea4
Sahilramjali <sahil.ramjali13@gmail.com>
2023-07-09 10:43:03 +0545

    commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+app.get('/api',(req,res)=>{
+    res.send(`<h5 style="color:green">
+        Hey Geek! you just deployed serverless express api</h5>`)
+})
+app.listen(8080,()=>{
+    console.log('Server started at http://localhost:8080')
+})
+module.exports=app
